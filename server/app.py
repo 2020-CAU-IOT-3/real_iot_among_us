55de28a11df3b3dd8750a2e62e09b76d20d3ffda
splo2t <yu990410@naver.com>
2020-11-14 23:35:36 +0900

    merge

--- server/app.py
+++ server/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, session
 from werkzeug.utils import secure_filename
 app = Flask(__name__, static_url_path='/static')
+
+missionGage = 0
+alive_list = ['red', 'blue', 'white', 'black']
 @app.route('/')
 def hello_world():
     return '<h1>among_us_server</h1>'
 
 @app.route('/missionStart')
-def missionComplete():
-    global mission_up
+def missionStart():
     if request.method == 'POST':
         f = request.files['file']
         f.save("imposterImage/" + secure_filename(f.filename))
@@ -16,15 +18,30 @@ def missionComplete():
         else :
             session['job'] = 'crew'
         return '<h1>success upload</h1>'
-    return '<h1>among_us_server</h1>'
+    else :
+        return render_template("mission_start.html")
 
 @app.route('/missionCompleteSend')
 def missionComplete():
+    global missionGage
+    if request.method == 'POST':
+        job = session['job']
+        if job == 'crew':
+            missionGage += 10
     return '<h1>among_us_server</h1>'
 
 @app.route('/missionCompleteShow')
 def missionCompleteShow():
-    return '<h1>among_us_server</h1>'
+    return '<h1>'+str(missionGage)+'</h1>'
+
+@app.route('/vote')
+def vote_page():
+    global alive_list
+    killed_people = 1
+    vote.vote(alive_list)
+
+    return '<h1>' + str(killed_people) + '</h1>'
+
 
 
 def isImposter(img):
