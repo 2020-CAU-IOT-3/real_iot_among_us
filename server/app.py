06c8b2503438c79c768caca5dd91b18799118402
splo2t <yu990410@naver.com>
2020-12-08 00:34:06 +0900

    merge

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 from werkzeug.utils import secure_filename
 import vote
 import socket
-missionGage = 0
+missionGage = 10
 alive_list = ['red', 'blue', 'white', 'black']
 app = Flask(__name__, static_url_path='/static')
 
@@ -12,11 +12,11 @@ def whoMission():
     if request.method == 'POST':
         returnjson = request.get_json(silent=True, cache=False, force=True)
         if returnjson['room'] == '0':
-            return "<h1>"+ str(room_mission[0]) + "</h1>"
+            return str(room_mission[0])
         elif returnjson['code'] == '1':
-            return "<h1>"+ str(room_mission[1]) + "</h1>"
+            return str(room_mission[1])
         elif returnjson['code'] == '2':
-            return "<h1>"+ str(room_mission[2]) + "</h1>"
+            return str(room_mission[2])
         return str(returnjson)
     if request.method == 'GET':
         return 'test'
@@ -38,20 +38,41 @@ def missionStart():
     else :
         return render_template("mission_start.html")
 """
-@app.route('/missionCompleteSend')
+@app.route('/missionCompleteSend', methods=['GET', 'POST'])
 def missionComplete():
     global missionGage
     if request.method == 'POST':
-            missionGage += 10
-    return '<h1>among_us_server</h1>'
-
-@app.route('/missionCompleteShow')
+        missionGage += 10
+        print(str(missionGage))
+        return str(missionGage)
+    else:
+        return render_template('mission_test.html')
+@app.route('/missionCompleteShow',  methods=['GET', 'POST'])
 def missionCompleteShow():
-    return '<h1>'+str(missionGage)+'</h1>'
+    global room_mission
+    max = 100
+    mission_bar = '['+ str('█')*missionGage
+    for x in range(missionGage,100):
+        mission_bar += '. '
+    mission_bar += ']'
+    if missionGage == 100:
+        return redirect(url_for('crewWin'))
+    return render_template('mission_now.html', progress = mission_bar)
+    #return '<h1>'+'room_0:'+ room_mission['0']+'</h1>'+ '<h1>'+'room_1:'+ room_mission['1']+'</h1>'+ '<h1>'+'mission_gage'+ mission_bar+'</h1>'
 
-@app.route('/missionCrewUpdate')
+@app.route('/crewWin',  methods=['GET', 'POST'])
+def crewWin():
+    return 'CrewWin'
+
+@app.route('/missionCrewUpdate',  methods=['GET', 'POST'])
 def missionCrewUpdate():
-    return '<h1>'+str(missionGage)+'</h1>'
+    global room_mission
+    if request.method == 'POST':
+        returnjson = request.get_json(silent=True, cache=False, force=True)
+        room_mission[returnjson['room']] = returnjson['type']
+        return 'complete:'+ str(room_mission[returnjson['room']])
+    else:
+        return '<h1>get</h1>'
 
 @app.route('/vote')
 def vote_page():
